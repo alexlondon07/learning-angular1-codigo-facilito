30e1292126700155a89dadfb93cb8cdf09f41f11
Alexander Londoño Espejo <alexander.londono@secuencia24.com>
2016-11-28 15:16:05 -0500

    AJAX con  2/2

--- http/controller.js
+++ http/controller.js
@@ -1,6 +1,7 @@
 var app = angular.module("MyFirsApp", []);
 app.controller("FirstController", function ($scope, $http) {
   $scope.posts = [];
+  $scope.newPost = {};
   $http.get("https://jsonplaceholder.typicode.com/posts")
       .success(function(data){
         $scope.posts = data;
@@ -9,4 +10,18 @@ app.controller("FirstController", function ($scope, $http) {
       .error(function(err){
         console.log(err);
       });
+  $scope.addPost =  function(){
+      $http.post("https://jsonplaceholder.typicode.com/posts",{
+          title: $scope.newPost.title,
+          body:  $scope.newPost.body,
+          userId: 1
+      })
+      .success(function(data, status, headers, config){
+        $scope.posts.push(data);
+        $scope.newPost = {};
+      })
+      .error(function(data, status, headers, config){
+        console.log(error);
+      });
+  }
 });
