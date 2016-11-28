54909e5f0561109d1995e6a876733073bbd11aa1
Alexander Londoño Espejo <alexander.londono@secuencia24.com>
2016-11-28 09:33:28 -0500

    Two way data binding y ng model.

--- hola_controller/controller.js
+++ hola_controller/controller.js
@@ -1,4 +1,19 @@
 var app = angular.module("MyFirsApp", []);
 app.controller("FirstController", function ($scope) {
-  $scope.nombre = "Alexander Londoño Espejo"
-})
+  $scope.name = "Alexander";
+  $scope.newComment = {};
+  $scope.comments = [
+    {
+      comment: "Excelente",
+      username: "alexlondon07"
+    },
+    {
+     comment: "Malo",
+      username: "alexlondon07---5"
+    }
+  ];
+  $scope.addComment = function(){
+    $scope.comments.push($scope.newComment);
+    $scope.newComment = {};
+  }
+});
